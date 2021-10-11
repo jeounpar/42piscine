d7b143cf98f5fe9d479a699f9bf6b3af4bfb5871
Jeoungkyu Park <jeounpar@c3r5s2.42seoul.kr>
2021-10-12 01:15:28 +0900

    solved ex10

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeounpar <jeounpar@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 00:23:35 by jeounpar          #+#    #+#             */
+/*   Updated: 2021/10/12 01:12:01 by jeounpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (src[cnt] != '\0')
+		cnt++;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (cnt);
+}
