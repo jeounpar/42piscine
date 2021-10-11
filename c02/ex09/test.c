d38cfba955e2cfbea9254250ab6e3c471ce173d3
Jeoungkyu Park <jeounpar@c3r5s2.42seoul.kr>
2021-10-11 23:57:48 +0900

    solved ex09

--- c02/ex09/test.c
+++ c02/ex09/test.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 16:39:46 by marvin            #+#    #+#             */
-/*   Updated: 2021/10/11 17:39:38 by marvin           ###   ########.fr       */
+/*   Updated: 2021/10/11 23:57:18 by jeounpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -14,21 +14,26 @@
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
-	int	use_upper;
+	int	vo_st;
 
 	i = 0;
-	use_upper = 1;
+	vo_st = 1;
 	while (str[i] != '\0')
 	{
-		if (str[i] < 'A' || (str[i] > 'Z' && str[i] < 'a') || str[i] > 'z')
-			use_upper = 1;
-		if (use_upper > 0 && (str[i] >= 'a' && str[i] <= 'z'))
+		if (str[i] < '0' || (str[i] > '9' && str[i] < 'A') || 
+				(str[i] > 'Z' && str[i] < 'a') || str[i] > 'z')
+			vo_st = 1;
+		else if (vo_st == 1)
 		{
-			use_upper = -1;
-			str[i] = str[i] - ('a' - 'A');
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - ('a' - 'A');
+			vo_st = 0;
 		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] = str[i] + ('a' - 'A');
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + ('a' - 'A');
+		}
 		i++;
 	}
 	return (str);
@@ -37,9 +42,9 @@ char	*ft_strcapitalize(char *str)
 int main(void)
 {
 	char str[] = "salut, Comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-
-	printf("%s@@\n", str);
-	printf("%s@@\n", ft_strcapitalize(str));
+	char src[] = "Salut, Comment Tu Vas ? 42mots Quarante-Deux; Cinquante+Et+Un";
+	printf("%s\n", ft_strcapitalize(str));
+	printf("%s\n", src);
 
 	return 0;
 }
