79755f408f75736dbfbb2956049ea3bedf0d7013
Park <cdex6531@ajou.ac.kr>
2021-10-19 00:41:36 +0900

    fix norm error

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -1,15 +1,15 @@
-int ft_is_prime(int nb)
+int	ft_is_prime(int nb)
 {
-    int n;
-    
-    n = 2;
-    if (nb < 2)
-        return (0);
-    while (n <= nb / 2)
-    {
-        if (nb % n == 0)
-            return (0);
-        n++;
-    }
-    return (1);
+	int	n;
+
+	n = 2;
+	if (nb < 2)
+		return (0);
+	while (n <= nb / 2)
+	{
+		if (nb % n == 0)
+			return (0);
+		n++;
+	}
+	return (1);
 }
--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -1,82 +1,84 @@
 #include <unistd.h>
 
-int solve_10queens(int row);
-int g_arr[10][10] = { 0, };
-int g_col[10] = { 0, };
-int g_dig[20] = { 0, };
-int g_dig2[20] = { 0, };
+int	solve_10queens(int row);
+int	g_arr[10][10] = {0, };
+int	g_col[10] = {0, };
+int	g_dig[20] = {0, };
+int	g_dig2[20] = {0, };
 
-int check(int row, int col)
+int	check(int row, int col)
 {
-    if (g_col[col])
-        return (0);
-    if (g_dig[row + col])
-        return (0);
-    if (g_dig2[row - col + 10])
-        return (0);
-    return (1);
+	if (g_col[col])
+		return (0);
+	if (g_dig[row + col])
+		return (0);
+	if (g_dig2[row - col + 10])
+		return (0);
+	return (1);
 }
 
-void    check_all(int row, int col, int *ans)
+void	check_all(int row, int col, int *ans)
 {
-    while(col < 10)
-    {
-        if (check(row, col))
-        {
-            g_dig[row + col] = 1;
-            g_dig2[row - col + 10] = 1;
-            g_col[col] = 1;
-            g_arr[row][col] = 1;
-            *ans += solve_10queens(row + 1);
-            g_dig[row + col] = 0;
-            g_dig2[row - col + 10] = 0;
-            g_col[col] = 0;
-            g_arr[row][col] = 0;
-        }
-        col++;
-    }
+	while (col < 10)
+	{
+		if (check(row, col))
+		{
+			g_dig[row + col] = 1;
+			g_dig2[row - col + 10] = 1;
+			g_col[col] = 1;
+			g_arr[row][col] = 1;
+			*ans += solve_10queens(row + 1);
+			g_dig[row + col] = 0;
+			g_dig2[row - col + 10] = 0;
+			g_col[col] = 0;
+			g_arr[row][col] = 0;
+		}
+		col++;
+	}
 }
 
-void    print_arr(void)
+void	print_arr(void)
 {
-    int     i;
-    int     j;
-    char    c;
+	int		i;
+	int		j;
+	char	c;
 
-    i = 0;
-    while (i < 10)
-    {
-        j = 0;
-        while (j < 10)
-        {
-            if(g_arr[j][i] == 1)
-            {
-                c = j + 48;
-                write(1, &c, 1);
-            }
-            j++;
-        }
-        i++;
-    }
-    write(1, "\n", 1);
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (g_arr[j][i] == 1)
+			{
+				c = j + 48;
+				write(1, &c, 1);
+			}
+			j++;
+		}
+		i++;
+	}
+	write(1, "\n", 1);
 }
 
-int solve_10queens(int row)
+int	solve_10queens(int row)
 {
-    int ans;
-    if (row == 10)
-    {
-        print_arr();
-        return 1;
-    }
-    ans = 0;
-    check_all(row, 0, &ans);
-    return (ans);
+	int	ans;
+
+	if (row == 10)
+	{
+		print_arr();
+		return (1);
+	}
+	ans = 0;
+	check_all(row, 0, &ans);
+	return (ans);
 }
 
-int ft_ten_queens_puzzle(void)
+int	ft_ten_queens_puzzle(void)
 {
-    int ans;
-    ans = solve_10queens(0);
-    return (ans);
+	int	ans;
+
+	ans = solve_10queens(0);
+	return (ans);
 }
