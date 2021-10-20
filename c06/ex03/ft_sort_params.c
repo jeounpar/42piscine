e3fbddb8865edcff8deece112b02820268db7c34
Park <cdex6531@ajou.ac.kr>
2021-10-20 14:10:15 +0900

    add c06 ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -12,7 +12,33 @@
 
 #include <unistd.h>
 
+void	ft_print(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
 int	main(int argc, char *argv[])
 {
-	
+	int	idx[argc];
+	int	i;
+	int	k;
+
+	i = 1;
+	while (i < argc)
+	{
+		idx[i] = i;
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		k = i;
+	}
 }
