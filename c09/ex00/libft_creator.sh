03e3acc2e254297ce8498d6393993dae998238cc
Jeoungkyu Park <jeounpar@c3r5s2.42seoul.kr>
2021-10-24 00:56:15 +0900

    complete C09

--- c09/ex00/libft_creator.sh
+++ c09/ex00/libft_creator.sh
@@ -6,12 +6,13 @@
 #    By: jeounpar <jeounpar@student.42seoul.kr      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/23 22:46:56 by jeounpar          #+#    #+#              #
-#    Updated: 2021/10/23 22:46:57 by jeounpar         ###   ########.fr        #
+#    Updated: 2021/10/24 00:54:52 by jeounpar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-gcc -c -Wall -Wextra -Werror *.c
-ar -r libft.a *.o
-rm -r *.o
+gcc -c -Wall -Wextra -Werror ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+ranlib libft.a
+rm ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
---
+++ c09/ex02/ft_split.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeounpar <jeounpar@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 19:42:32 by jeounpar          #+#    #+#             */
+/*   Updated: 2021/10/23 17:28:33 by jeounpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	is_ok(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i] != '\0')
+	{
+		if (c == charset[i] || c == '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	cnt_word(char *str, char *charset)
+{
+	int	cnt;
+	int	i;
+
+	cnt = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] != '\0' && (is_ok(str[i], charset)))
+			i++;
+		if (str[i] != '\0' && !(is_ok(str[i], charset)))
+		{
+			cnt++;
+			while (str[i] != '\0' && !(is_ok(str[i], charset)))
+				i++;
+		}
+	}
+	return (cnt);
+}
+
+void	words_malloc(char *str, char *charset, char **words)
+{
+	int	i;
+	int	j;
+	int	idx;
+
+	i = 0;
+	j = 0;
+	idx = 0;
+	while (str[i] != '\0')
+	{
+		if (!is_ok(str[i], charset))
+		{
+			j++;
+			if (is_ok(str[i + 1], charset))
+			{
+				words[idx] = (char *)malloc((j + 1) * sizeof(char));
+				idx++;
+				j = 0;
+			}
+		}
+		i++;
+	}
+}
+
+void	wr_words(char *str, char *charset, char **words)
+{
+	int	i;
+	int	j;
+	int	idx;
+
+	i = 0;
+	j = 0;
+	idx = 0;
+	while (str[i] != '\0')
+	{
+		if (!is_ok(str[i], charset))
+		{
+			words[idx][j] = str[i];
+			j++;
+			if (is_ok(str[i + 1], charset))
+			{
+				words[idx][j] = '\0';
+				idx++;
+				j = 0;
+			}
+		}
+		i++;
+	}
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	int		cnt;
+	char	**words;
+
+	cnt = cnt_word(str, charset);
+	words = (char **)malloc((cnt + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	words_malloc(str, charset, words);
+	wr_words(str, charset, words);
+	words[cnt] = 0;
+	return (words);
+}
